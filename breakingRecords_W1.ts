f29c9bf7a9cd534f0ed7cee94c33781b79ee96cb
Mbiydzenyuy Stephen <mbiystephjr@gmail.com>
2025-03-26 17:56:54 +0100

    Enhance comments in breakingRecords function to clarify problem objective and algorithm steps

--- breakingRecords_W1.ts
+++ breakingRecords_W1.ts
@@ -1,7 +1,9 @@
-function breakingRecords(scores: number[]): number[] {
-    // Write your code here
-    //OBJECTIVE
-    //To find the minimum count and maximum count of two arrarys
+// Name of the Problem: Breaking the Records
+
+    //OBJECTIVE OF THE PROBLEM
+    //The of objective of this problem is to find the number of times
+    //  the minimum and maximum score was broken
+
     //ALGORITHM
     //1. Check if the array is empty and then return an array with two values[0,0]
     //2. Declare variables to hold the following
@@ -9,6 +11,8 @@ function breakingRecords(scores: number[]): number[] {
     //3. compare the numbers in the array with maximum and any time that a number from the array is greater than maximum, we increament our maximum count.
     //4. compare the numbers in the array with the minimum, anytime a number in the array is less than the minimum, we increment the minimum count
     //5. We finally then return maxCount and Mincount
+
+function breakingRecords(scores: number[]): number[] {
     
     if(scores.length === 0 ) return [0,0]
     
