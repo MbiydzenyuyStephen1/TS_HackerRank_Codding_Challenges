829a1ac2e786d7e2243ee288312566dc30c457ba
Mbiydzenyuy Stephen <mbiystephjr@gmail.com>
2025-03-26 18:06:18 +0100

    Enhance comments in timeConversion function for clarity on problem name, objective, and algorithm steps

--- timeConversion_W1.ts
+++ timeConversion_W1.ts
@@ -1,15 +1,20 @@
-function timeConversion(s: string): string {
-    // Write your code here
- //Objective: convert from from time from 12hr time to 24hr time
+//NAME OF THE PROBLEM: Time Conversion
+
+    //Objective of the problem 
+        //convert from from time from 12hr time to 24hr time
+
+
     //ALGORITHM
-    //1. Get the last digits of the time
-    //2. Get the the time excluding last two digits
-    //3. Split the time
-    //4. convert the split at index zero to number
-    //5. where the value in period is AM and hour at index Zero is 12, hour takes 0
-    //6. Else if hour is not 12 we add what ever is in hour with 12
-    //7.Convert hour back to string
-    
+        //1. Get the last digits of the time
+        //2. Get the the time excluding last two digits
+        //3. Split the time
+        //4. convert the split at index zero to number
+        //5. where the value in period is AM and hour at index Zero is 12, hour takes 0
+        //6. Else if hour is not 12 we add what ever is in hour with 12
+        //7.Convert hour back to string
+
+function timeConversion(s: string): string {
+
     const lastDigit: string = s.slice(-2);
     
     const timeExcludingLastCharacters: string = s.slice(0,-2);
